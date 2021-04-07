e28c22b64ac0167399216518ea3f9349b6d3331e
luan.nguyen <luan.nguyen@cyberlogitec>
2021-04-07 15:19:07 +0700

    Implement Base Test for support multiple browsers

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,6 +1,31 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
 // Class chứa các hàm dùng chung cho các tầng testcases (class trong testcases)
 public class BaseTest {
+    WebDriver driver;
+    String prjLocation = System.getProperty("user.dir");
 
+    public WebDriver getBrowserDriver(String browserName) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", prjLocation + "\\browserDrivers\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", prjLocation + "\\browserDrivers\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("edge_chormium")) {
+            System.setProperty("webdriver.edge.driver", prjLocation + "\\browserDrivers\\msedgedriver.exe");
+            driver = new EdgeDriver();
+        } else {
+            throw new RuntimeException("Not found the browser name!");
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        return driver;
+    }
 }
---
+++ testcases/com/wordpress/posts/Level_4_Login_Multiple_Browsers.java
@@ -0,0 +1,103 @@
+package com.wordpress.posts;
+
+import commons.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pageObjects.wordpress.DashboardPageObject;
+import pageObjects.wordpress.LoginPageObject;
+
+public class Level_4_Login_Multiple_Browsers extends BaseTest {
+
+    WebDriver driver;
+    LoginPageObject loginPage;
+    DashboardPageObject dashboardPage;
+
+    @Parameters("browser")
+    @BeforeClass
+    public void BeforeClass(String browserName) {
+        driver = getBrowserDriver(browserName);
+    }
+
+    @Parameters("url")
+    @BeforeMethod
+    public void beforeMethod(String url) {
+        driver.get(url);
+        loginPage = new LoginPageObject(driver);
+    }
+
+    @Test
+    public void Login_01_Empty_Email_Username() {
+        loginPage.inputToUsernameTextbox("");
+        loginPage.clickToContinueButton();
+
+        Assert.assertEquals(loginPage.isErrorMessageDisplayed(), "Please enter a username or email address.");
+    }
+
+	@Test
+	public void Login_02_Invalid_Email_Username() {
+		loginPage.inputToUsernameTextbox("auto@@");
+		loginPage.clickToContinueButton();
+
+		Assert.assertEquals(loginPage.isErrorMessageDisplayed(),
+				"User does not exist. Would you like to create a new account?");
+	}
+
+//	@Test
+	public void Login_03_Email_Username_Not_Exist() {
+		loginPage.inputToUsernameTextbox("auto@auto.com");
+		loginPage.clickToContinueButton();
+
+		Assert.assertEquals(loginPage.isErrorMessageDisplayed(),
+				"User does not exist. Would you like to create a new account?");
+	}
+
+//	@Test
+	public void Login_04_Empty_Password() {
+		loginPage.inputToUsernameTextbox("automationeditor");
+		loginPage.clickToContinueButton();
+		loginPage.inputToPasswordTextbox("");
+		loginPage.clickToLoginButton();
+
+		Assert.assertEquals(loginPage.isErrorMessageDisplayed(),
+				"Don't forget to enter your password.");
+	}
+
+//	@Test
+	public void Login_05_Invalid_Password() {
+		loginPage.inputToUsernameTextbox("automationeditor");
+		loginPage.clickToContinueButton();
+		loginPage.inputToPasswordTextbox("1asd");
+		loginPage.clickToLoginButton();
+
+		Assert.assertEquals(loginPage.isErrorMessageDisplayed(),
+				"Oops, that's not the right password. Please try again!");
+	}
+
+//	@Test
+	public void Login_06_Incorrect_Password() {
+		loginPage.inputToUsernameTextbox("automationeditor");
+		loginPage.clickToContinueButton();
+		loginPage.inputToPasswordTextbox("1asd");
+		loginPage.clickToLoginButton();
+
+		Assert.assertEquals(loginPage.isErrorMessageDisplayed(),
+				"Oops, that's not the right password. Please try again!");
+	}
+
+//	@Test
+	public void Login_07_Valid_Email_Password() {
+		loginPage.inputToUsernameTextbox("automationeditor");
+		loginPage.clickToContinueButton();
+		loginPage.inputToPasswordTextbox("automationfc");
+		loginPage.clickToLoginButton();
+
+		dashboardPage = new DashboardPageObject(driver);
+		Assert.assertTrue(dashboardPage.isDashboardHeaderTextDisplayed());
+	}
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
+}
