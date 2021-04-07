d46cb0e518b26e4fe598b8430f5fa900fbf29aca
luan.nguyen <luan.nguyen@cyberlogitec>
2021-04-07 17:30:47 +0700

    Update- Implement Base Test for support multiple browsers

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -5,27 +5,62 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
-// Class chứa các hàm dùng chung cho các tầng testcases (class trong testcases)
 public class BaseTest {
-    WebDriver driver;
-    String prjLocation = System.getProperty("user.dir");
+    private WebDriver driver;
+    private String prjLocation = System.getProperty("user.dir");
+    private String osName = System.getProperty("os.name");
 
-    public WebDriver getBrowserDriver(String browserName) {
+    protected WebDriver getBrowserDriver(String browserName) {
+        setBrowserDriver();
         if (browserName.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", prjLocation + "\\browserDrivers\\chromedriver.exe");
             driver = new ChromeDriver();
         } else if (browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", prjLocation + "\\browserDrivers\\geckodriver.exe");
             driver = new FirefoxDriver();
         } else if (browserName.equalsIgnoreCase("edge_chormium")) {
-            System.setProperty("webdriver.edge.driver", prjLocation + "\\browserDrivers\\msedgedriver.exe");
             driver = new EdgeDriver();
         } else {
             throw new RuntimeException("Not found the browser name!");
         }
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         return driver;
+    }
+
+    private void setBrowserDriver() {
+        String browserFolderPath = prjLocation + getDirectorySlash("browserDrivers");
+        if (isWindow()) {
+            System.setProperty("webdriver.chrome.driver", browserFolderPath + "chromedriver.exe");
+            System.setProperty("webdriver.gecko.driver", browserFolderPath + "geckodriver.exe");
+            System.setProperty("webdriver.edge.driver", browserFolderPath + "msedgedriver.exe");
+        } else if (isMac()) {
+            System.setProperty("webdriver.chrome.driver", browserFolderPath + "chromedriver_mac");
+            System.setProperty("webdriver.gecko.driver", browserFolderPath + "geckodriver_mac");
+            System.setProperty("webdriver.edge.driver", browserFolderPath + "msedgedriver_mac");
+        }
+    }
+
+    private String getDirectorySlash(String folderName) {
+        if (isMac() || isUnix()) {
+            folderName = "/" + folderName + "/";
+        } else if (isWindow()) {
+            folderName = "\\" + folderName + "\\";
+        } else {
+            folderName = null;
+        }
+        return folderName;
+    }
+
+    private boolean isWindow() {
+        return (osName.toLowerCase().indexOf("win") >= 0);
+    }
+
+    private boolean isMac() {
+        return (osName.toLowerCase().indexOf("mac") >= 0);
+    }
+
+    private boolean isUnix() {
+        return (osName.toLowerCase().indexOf("nix") >= 0);
     }
 }
